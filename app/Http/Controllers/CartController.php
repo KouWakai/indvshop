26157f5375f654de6a6a4171ba1dad2ec5e769e6
若井 光大 <wakaikoudai@wakai-no-MacBook-Air.local>
2021-04-30 18:58:48 +0900

    カートが空のときのメッセージを表示した

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,11 +8,21 @@ use App\Models\Product;
 
 class CartController extends Controller
 {
+    public function index(){
+        return view('cart.index');
+    }
+
     public function store(){
         $product = Product::find(request()->input('id'));
-        
+
         \Cart::add(['id' => $product->id, 'name' => $product->caption, 'qty' => 1, 'price' => $product->price, 'weight' => 0,'options' => ['image' => $product->image]]);
 
         return view('cart.index');
+    }
+
+    public function destroy($rowId){
+        \Cart::remove($rowId);
+
+        return redirect('/cart');
     }
 }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row p-3">
+    <div class="row pt-3">
         <div class="col-9 d-flex justify-content-between border-bottom bg-white text-center">
             <div class="col-2">商品画像</div>
             <div class="col-2">商品名</div>
@@ -10,7 +10,18 @@
             <div class="col-2">価格</div>
             <div class="col-1"> </div>
         </div>
-        <div>
+        <div class="col-3">
+        </div>
+    </div>
+        <div class="row">
+            @if(Cart::content()->isEmpty())
+                <div class="col-9 d-flex justify-content-center border bg-white">
+                    <h4 class="col-9 p-4 text-center">カートは空です</h4>
+                </div>
+
+            @else
+            
+
             @foreach(Cart::content() as $row)
             <div class="col-9 align-items-center d-flex justify-content-between text-center border-bottom bg-white">
                 <img src={{ $row->options->image }} class="col-2 p-3">
@@ -22,9 +33,10 @@
                     @method('DELETE')
                     <button class="btn btn-outline-danger">削除</button>
                 </form>
-            </div>
+            </div>                
+            
             @endforeach
+            @endif
         </div>
-    </div>
 </div>
 @endsection
