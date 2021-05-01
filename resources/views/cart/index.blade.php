bcb03d00fcdb7e3678ee125958ffb1e7f36625d9
KouWakai <kouyama112@gmail.com>
2021-05-01 17:41:54 +0900

    [UI] カートのボーダーを修正した

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row pt-3">
-        <div class="col-9 d-flex justify-content-between border-bottom bg-white text-center">
+        <div class="col-9 d-flex justify-content-between bg-white text-center">
             <div class="col-2">商品画像</div>
             <div class="col-2">商品名</div>
             <div class="col-2">数量</div>
@@ -23,7 +23,7 @@
             
 
             @foreach(Cart::content() as $row)
-            <div class="col-9 align-items-center d-flex justify-content-between text-center border-bottom bg-white">
+            <div class="col-9 align-items-center d-flex justify-content-between text-center border-top border-bottom bg-white">
                 <img src={{ $row->options->image }} class="col-2 p-3">
                 <div class="col-2">{{ $row->name }}</div>
                 <div class="col-2">1</div>
