afd63996cb4de8d29820d6f99d8cdca4f04d4423
KouWakai <kouyama112@gmail.com>
2021-05-03 22:35:33 +0900

    [Backend] コミット漏れ　管理画面のコントローラーにviewに渡す値を設定した

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
         $path = $request->path();
+
+        if($path = 'admin/users'){
+            $users = User::all();
+            return view($path, compact('users'));
+        }
+
         return view($path);
     }
 }
