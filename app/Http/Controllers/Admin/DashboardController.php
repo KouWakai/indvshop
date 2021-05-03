00fdc404cd7e919effecc21612aa427abe67a8bd
KouWakai <kouyama112@gmail.com>
2021-05-03 22:39:08 +0900

    [Controller] pathの評価が必ず真になっていたので修正した

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -12,7 +12,7 @@ class DashboardController extends Controller
     {
         $path = $request->path();
 
-        if($path = 'admin/users'){
+        if($path == 'admin/users'){
             $users = User::all();
             return view($path, compact('users'));
         }
