d7813925ead5ea4c429be4397f8869ae7e82d0c7
若井 光大 <wakaikoudai@wakai-no-MacBook-Air.local>
2021-05-05 19:18:48 +0900

    [VIEW] カートのインデックスを変更した

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -3,31 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row pt-3">
-        <div class="col-9 d-flex justify-content-between bg-white text-center">
-            <div class="col-2">商品画像</div>
-            <div class="col-2">商品名</div>
-            <div class="col-2">数量</div>
-            <div class="col-2">価格</div>
-            <div class="col-1"> </div>
+        <div class="col-12 d-flex justify-content-between bg-white text-center border rounded-top border-bottom-0 align-items-center py-2">
+            <span class="col-2">商品画像</span>
+            <span class="col-2">商品名</span>
+            <span class="col-2">数量</span>
+            <span class="col-2">価格</span>
+            <form class="col-2" action="/cart/all" method="post">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm" name="empty" value="1">カートを空にする</button>
+            </form>
         </div>
         <div class="col-3">
         </div>　
     </div>
         <div class="row">
             @if(Cart::content()->isEmpty())
-                <div class="col-9 d-flex justify-content-center border bg-white">
+                <div class="col-12 d-flex justify-content-center border rounded-bottom bg-white">
                     <h4 class="col-9 mb-0 p-4 text-center">カートは空です</h4>
                 </div>
 
             @else
 
             @foreach(Cart::content() as $row)
-            <div class="col-9 align-items-center d-flex justify-content-between text-center border-top border-bottom bg-white">
+            <div class="col-12 align-items-center d-flex justify-content-between text-center border rounded-bottom bg-white">
                 <img src="/storage/{{ $row->options->image }}" class="col-2 p-3">
-                <div class="col-2">{{ $row->name }}</div>
-                <div class="col-2">1</div>
-                <div class="col-2">{{ $row->price }}</div>
-                <form action="/cart/{{ $row->rowId }}" method="post">
+                <span class="col-2">{{ $row->name }}</span>
+                <span class="col-2">1</span>
+                <span class="col-2">{{ $row->price }}</span>
+                <form class="col-2" action="/cart/{{ $row->rowId }}" method="post">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-outline-danger">削除</button>
@@ -36,11 +40,10 @@
 
             @endforeach
 
-            <div class="col-3">
-                <button class="btn btn-danger btn-block mb-4">カートを空にする</button>
-                <div class="d-flex align-items-center border-bottom">
-                    <h4 class="text-left pl-4">合計:</h4>
-                    <h5 class="px-4">{{ Cart::total() }}円</h5>
+            <div class="col-4 pt-3 ml-auto">
+                <div class="m-0 py-3 d-flex align-items-center border-top border-bottom">
+                    <h4 class="m-0 text-left pl-4">合計:</h4>
+                    <h5 class="m-0 px-4">{{ Cart::total() }}円</h5>
                 </div>
                 <button class="btn btn-success btn-lg btn-block my-3">会計に進む</button>
             </div>
