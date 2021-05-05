4812dd2dc35672a5b54768ce9f071978bbf5c299
若井 光大 <wakaikoudai@wakai-no-MacBook-Air.local>
2021-05-05 19:19:51 +0900

    [Route] カートを空にするボタンを追加した

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::delete('/cart/{rowId}', [CartController::class, 'destroy']);
+Route::delete('/cart/all', [CartController::class, 'destroy']);
 Route::get('/cart', [CartController::class, 'index']);
 Route::post('/cart', [CartController::class, 'store']);
 
