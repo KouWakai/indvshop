a6b5a367fadb52ab57d5b1b651c1378e8c7839f1
KouWakai <kouyama112@gmail.com>
2021-05-07 01:22:38 +0900

    [VIEW] ユーザーがログイン時の会計画面を作成した

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+
+@guest
 <div class="container">
       <div class="row py-5">
     <div class="col-md-4 order-md-2 mb-4">
@@ -201,5 +203,141 @@
     </div>
   </div>
 </div>
+@else
+<div class="container">
+      <div class="row py-5">
+    <div class="col-md-4 order-md-2 mb-4">
+      <h4 class="d-flex justify-content-between align-items-center mb-3">
+        <span class="text-muted">カート内の商品</span>
+        <span class="badge badge-secondary badge-pill">{{ Cart::count() }}</span>
+      </h4>
+      <ul class="list-group mb-3">
+
+      @foreach(Cart::content() as $row)
+        <li class="list-group-item d-flex justify-content-between lh-condensed">
+          <div>
+            <h6 class="my-0">{{ $row->name }}</h6>
+            <small class="text-muted"></small>
+          </div>
+          <span class="text-muted">{{ $row->price }}円</span>
+        </li>
+      @endforeach
+
+        <li class="list-group-item d-flex justify-content-between">
+          <span>合計 (税込み)</span>
+          <strong>{{ Cart::total() }}円</strong>
+        </li>
+      </ul>
+
+      </form>
+    </div>
+    <div class="col-md-8 order-md-1">
+      <h4 class="mb-3">お客様情報</h4>
+      <form action="cart/checkout/ordered" method="post" class="needs-validation" novalidate="">
+
+        <div class="mb-3">
+          <label class="font-weight-bold" for="username">お名前</label>
+          <div class="input-group">
+            <span class="offset-1">{{ $user->name }}</span>
+            <div class="invalid-feedback" style="width: 100%;">
+              Your username is required.
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label class="font-weight-bold" for="email">Eメールアドレス</label>
+          <div>
+            <span class="offset-1">{{ $user->email }}</span>
+          </div>
+          <div class="invalid-feedback">
+            Please enter a valid email address for shipping updates.
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label class="font-weight-bold" for="address">住所</label>
+            <div>
+              <span class="offset-1">{{ $user->address1 }} {{ $user->address2 }} {{ $user->address3 }}</span>
+            </div>
+          <div class="invalid-feedback">
+            Please enter your shipping address.
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label class="font-weight-bold" for="phone">電話番号</label>
+          <div>
+            <span class="offset-1">{{ $user->phone }}</span>
+          </div>
+        </div>
+
+
+        <hr class="mb-4">
+        <div class="custom-control custom-checkbox">
+          <input type="checkbox" class="custom-control-input" id="same-address">
+          <label class="custom-control-label" for="same-address">請求先住所はお届け先住所と同じ</label>
+        </div>
+        <hr class="mb-4">
+
+        <h4 class="mb-3">お支払い方法</h4>
+
+        <div class="d-block my-3">
+          <div class="custom-control custom-radio" data-toggle="dropdown">
+            <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked="" required="">
+            <label class="custom-control-label" for="credit">クレジットカード</label>
+          </div>
+          <div class="custom-control custom-radio">
+            <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="">
+            <label class="custom-control-label" for="debit">銀行振込</label>
+          </div>
+        </div>
+
+          <div class="row">
+            <div class="col-md-6 mb-3">
+              <label for="cc-name">クレジットカード名義</label>
+              <input type="text" class="form-control" id="cc-name" placeholder="" required="">
+              <small class="text-muted">性・名をアルファベットで</small>
+              <div class="invalid-feedback">
+                Name on card is required
+              </div>
+            </div>
+            <div class="col-md-6 mb-3">
+              <label for="cc-number">クレジットカード番号</label>
+              <input type="text" class="form-control" id="cc-number" placeholder="" required="">
+              <div class="invalid-feedback">
+                Credit card number is required
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3 mb-3">
+              <label for="cc-expiration">有効期限</label>
+              <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
+              <div class="invalid-feedback">
+                Expiration date required
+              </div>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="cc-cvv">CVV</label>
+              <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
+              <div class="invalid-feedback">
+                Security code required
+              </div>
+            </div>
+          </div>
+          <hr class="mb-4">
+              <div class="form-group row mb-0 text-center">
+                  <div class="col-md-6 offset-md-3">
+                      <button type="submit" class="btn btn-primary">
+                          注文を確定する
+                      </button>
+                  </div>
+              </div>
+      </form>
+    </div>
+  </div>
+</div>
+@endguest
 
 @endsection
