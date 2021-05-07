84581043e3a162fd8ee7d0c36a16bd18d6fc5166
KouWakai <kouyama112@gmail.com>
2021-05-07 23:49:17 +0900

    [VIEW] 注文管理画面に注文された商品を確認するモーダルウィンドウを追加した

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -15,7 +15,7 @@
               <th>支払い方法</th>
               <th>支払い状況</th>
               <th>注文合計金額</th>
-              <th>編集</th>
+              <th>注文商品</th>
             </tr>
           </thead>
           <tbody>
@@ -33,13 +33,62 @@
               </td>
               <td>{{ $ord->total }}</td>
               <td>
-                <a href="">
-                    <span data-feather="edit-2" style="width: 18px; height: 18px"></span>
-                </a>
+                <div data-toggle="modal" data-target="#ShowModal-{{ $ord->id }}" value="{{ $ord }}">
+                    <span data-feather="eye" style="width: 18px; height: 18px"></span>
+                </div>
               </td>
             </tr>
+
+            <div class="modal fade" id="ShowModal-{{ $ord->id }}" tabindex="-1" role="dialog" aria-labelledby="ShowModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="ShowModalLabel">注文詳細</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                        <?php
+                        $order = App\Models\Order::with('product')->find($ord->id);
+                        ?>
+                        @foreach ($order->product as $ord)
+                            <div class="row">
+                                <label class="col-md-4 font-weight-bold">商品{{ $loop->iteration }}</label>
+                            </div>
+                            <div class="row">
+                                <span class="offset-1">{{ $ord->caption }}</span>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-4 font-weight-bold">数量</label>
+                            </div>
+                            <div class="row">
+                                <span class="offset-1">{{ $ord->pivot->quantity }}</span>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-4 font-weight-bold">単価</label>
+                            </div>
+                            <div class="row">
+                                <span class="offset-1">{{ $ord->price }}円</span>
+                            </div>
+                        @endforeach
+
+
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+
             @endforeach
           </tbody>
         </table>
       </div>
+
+
+
+
 @endsection
