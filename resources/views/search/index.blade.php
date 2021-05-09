6f22bb0a6c182ea6d1468a6d605b6960ac2ec229
KouWakai <kouyama112@gmail.com>
2021-05-10 02:55:26 +0900

    [VIEW]商品ページに検索機能を実装した

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -24,17 +24,16 @@
         </form>
       </div>
 
-
         <div class="row d-flex justify-content-left">
-            @foreach($product as $prd)
+            @foreach($result as $rlt)
                 <div class="col-4 mt-2 py-3">
-                    <a href="/product/{{ $prd->id }}">
-                        <img src="/storage/{{ $prd->image ?? ''}}" alt="" class="w-100 border rounded-top border-bottom-0">
+                    <a href="/product/{{ $rlt->id }}">
+                        <img src="/storage/{{ $rlt->image ?? ''}}" alt="" class="w-100 border rounded-top border-bottom-0">
                     </a>
-                    <a href="/product/{{ $prd->id }}">
+                    <a href="/product/{{ $rlt->id }}">
                         <div class="text-center border rounded-bottom bg-white text-dark border-top-0">
-                        <span>{{ $prd->caption ?? ''}}<br></span>
-                        <span>¥{{ $prd->price ?? ''}}</span>
+                        <span>{{ $rlt->caption ?? ''}}<br></span>
+                        <span>¥{{ $rlt->price ?? ''}}</span>
                         </div>
                     </a>
                 </div>
