6dba96ebd427b83e6b3f08cb2c31546fe6d8d5e1
KouWakai <kouyama112@gmail.com>
2021-05-09 20:27:04 +0900

    [CICD] config

--- config/database.php
+++ config/database.php
@@ -91,7 +91,7 @@ return [
             'prefix_indexes' => true,
         ],
 
-        'circle_testing' => [
+        'circle_test' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
