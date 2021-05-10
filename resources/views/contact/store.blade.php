354321571babc6b5ae401e6cc89a975ee4d161f9
KouWakai <kouyama112@gmail.com>
2021-05-11 05:19:32 +0900

    [VIEW] 送信完了画面を実装した

--- resources/views/contact/store.blade.php
+++ resources/views/contact/store.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div class="container">
-   success
+ <div class="row justify-content-center">
+        <div class="col-md-4 my-4">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    <h5 class="p-2 m-0">お問い合わせの受付が完了しました</h5>
+                </div>
+
+                <div class="card-body">
+                        <div class="text-center">
+                           <a href="/home"><span>ホームに戻る</span></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 @endsection
--- resources/views/customorder/store.blade.php
+++ resources/views/customorder/store.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 <div class="container">
-   success
+ <div class="row justify-content-center">
+        <div class="col-md-4 my-4">
+            <div class="card mt-5">
+                <div class="card-header text-center">
+                    <h5 class="p-2 m-0">見積もりの受付が完了しました</h5>
+                </div>
+
+                <div class="card-body">
+                        <div class="text-center">
+                           <a href="/home"><span>ホームに戻る</span></a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
