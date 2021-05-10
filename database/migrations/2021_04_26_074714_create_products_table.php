013c9eb3659a4da41954614b8c9633b23360878b
KouWakai <kouyama112@gmail.com>
2021-05-11 01:34:41 +0900

    [MIGRATION] 商品のカテゴリ検索機能を追加した

--- database/migrations/2021_04_26_074714_create_products_table.php
+++ database/migrations/2021_04_26_074714_create_products_table.php
@@ -18,6 +18,7 @@ class CreateProductsTable extends Migration
             $table->string('caption');
             $table->string('image');
             $table->unsignedBigInteger('price');
+            $table->string('category');
             $table->timestamps();
         });
     }
