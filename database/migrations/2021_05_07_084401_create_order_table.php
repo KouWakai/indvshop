4bc0408f7dd728d33a8f11a21d1af44f571ae493
KouWakai <kouyama112@gmail.com>
2021-05-10 22:42:20 +0900

    [MIGRATION] 商品管理画面にページネーションを実装した

--- database/migrations/2021_05_07_084401_create_order_table.php
+++ database/migrations/2021_05_07_084401_create_order_table.php
@@ -15,10 +15,17 @@ class CreateOrderTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->nullable();
             $table->string('payment');
             $table->boolean('paid');
             $table->unsignedInteger('total');
+            $table->string('username')->nullable();
+            $table->string('email')->nullable();
+            $table->string('prefecture')->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('address3')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
